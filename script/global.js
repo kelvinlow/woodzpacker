779bc88dd7271286e2907c8313c1bbcef6c234c9
Kelvin Low <shin-sheng_low@astro.com.my>
2025-03-24 09:27:33 +0800

    Add anmination for menu toggle

--- script/global.js
+++ script/global.js
@@ -1,2 +1,21 @@
-document.getElementById("nav-placeholder").innerHTML = fetch('/components/nav.html').then(response => response.text()).then(html => document.getElementById("nav-placeholder").innerHTML = html);
-document.getElementById("footer-placeholder").innerHTML = fetch('/components/footer.html').then(response => response.text()).then(html => document.getElementById("footer-placeholder").innerHTML = html);
+document.getElementById("nav-placeholder").innerHTML = fetch('/components/nav.html')
+  .then(response => response.text())
+  .then(html => {
+    console.log("Navigation Bar")
+    document.getElementById("nav-placeholder").innerHTML = html;
+
+    const mobileMenu = document.getElementById('nav-icon3');
+    const navLinks = document.getElementById('nav-links');
+    mobileMenu.addEventListener('click', () => {
+      mobileMenu.classList.toggle('open')
+      navLinks.classList.toggle('active');
+    });
+  });
+document.getElementById("footer-placeholder").innerHTML = fetch('/components/footer.html')
+  .then(response => response.text())
+  .then(html => {
+    console.log("Footer Bar")
+    document.getElementById("footer-placeholder").innerHTML = html;
+});
+
+
